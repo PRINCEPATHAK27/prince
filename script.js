034f767e3e18b9dde45a33e1da3c1af52464c6e1
PRINCEPATHAK27 <princepathak434343@gmail.com>
2026-08-01 13:08:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,8 @@ function openProposal(){
     window.location.href = "proposal.html";
 
 }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("sw.js");
+  });
+}
